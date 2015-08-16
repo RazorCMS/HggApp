3b33925dfe09eb4feaf2c761fd18c7c7de6f8a99
SiXie <Si.Xie@cern.ch>
2015-08-16 23:39:11 +0200

    update mgg shape study

--- SusyHgg/scripts/ForRun1Paper/HighPtMggShapeStudy.C
+++ SusyHgg/scripts/ForRun1Paper/HighPtMggShapeStudy.C
@@ -46,7 +46,8 @@ void PlotMggShapeVsRsq(bool useMC = false, string category = "highres") {
 
   string controlRegionCutString = "";
   if (useMC) controlRegionCutString = "";
-  else controlRegionCutString = "&& (!pho1_pass_iso || !pho2_pass_iso) ";
+  //else controlRegionCutString = "&& (!pho1_pass_iso || !pho2_pass_iso) ";
+  else controlRegionCutString = "&& pho1_pass_iso && pho2_pass_iso ";
 
   tree->Draw("mgg>>MGG_MR150ToInf_Rsq0p00ToInf", ("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 150 && t1Rsq > 0.0 " + categoryCutString + controlRegionCutString).c_str(),"");
   tree->Draw("mgg>>MGG_MR400ToInf_Rsq0p00To0p015",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 400 && t1Rsq > 0.0 && t1Rsq<0.015 " + categoryCutString + controlRegionCutString).c_str(),"");
@@ -73,7 +74,7 @@ void PlotMggShapeVsRsq(bool useMC = false, string category = "highres") {
  MGG_MR400ToInf_Rsq0p03To0p05->SetStats(0);
  MGG_MR400ToInf_Rsq0p05ToInf->SetStats(0);
  MGG_MR400ToInf_Rsq0p00To0p015->Draw("hist");
- MGG_MR400ToInf_Rsq0p00To0p015->SetMaximum(0.3);
+ MGG_MR400ToInf_Rsq0p00To0p015->SetMaximum(0.15);
  MGG_MR400ToInf_Rsq0p00To0p015->SetMinimum(0.0);
  MGG_MR400ToInf_Rsq0p00To0p015->GetXaxis()->SetTitleSize(0.045);
  MGG_MR400ToInf_Rsq0p00To0p015->GetYaxis()->SetTitleSize(0.045);
@@ -142,38 +143,36 @@ void PlotMggShapeVsMR(bool useMC = false, string category = "highres") {
   if (!useMC) {
     fileData = new TFile("/afs/cern.ch/user/s/sixie/eos/cms/store/group/phys_susy/razor/run1/HggRazor/AlexRaidDisk/DoublePhoton_22Jan2013_Run2012ABCD_v9.root","READ");
   } else {
-    fileData = new TFile("/afs/cern.ch/user/s/sixie/eos/cms/store/group/phys_susy/razor/run1/HggRazor/AlexRaidDisk/DiPhotonJetsBox_M60_8TeV-sherpa__Summer12_DR53X-PU_S10_START53_V7C-v1_v9__WEIGHTED.root","READ");
+    //fileData = new TFile("/afs/cern.ch/user/s/sixie/eos/cms/store/group/phys_susy/razor/run1/HggRazor/AlexRaidDisk/DiPhotonJetsBox_M60_8TeV-sherpa__Summer12_DR53X-PU_S10_START53_V7C-v1_v9__WEIGHTED.root","READ");
+    fileData = new TFile("/afs/cern.ch/user/s/sixie/eos/cms/store/group/phys_susy/razor/run1/HggRazor/AlexRaidDisk/GJet_Pt40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_v9.root","READ");
   }
  
   TTree *tree = (TTree*)fileData->Get("SusyHggTree");
   
-  TH1D *MGG_MR150ToInf_Rsq0p00ToInf = new TH1D("MGG_MR150ToInf_Rsq0p00ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 30,100,400);
-  TH1D *MGG_MR150To200_Rsq0p05ToInf = new TH1D("MGG_MR150To200_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 30,100,400);
-  TH1D *MGG_MR200To300_Rsq0p05ToInf = new TH1D("MGG_MR200To300_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 30,100,400);
-  TH1D *MGG_MR300To400_Rsq0p05ToInf = new TH1D("MGG_MR300To400_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 30,100,400);
-  TH1D *MGG_MR400ToInf_Rsq0p05ToInf = new TH1D("MGG_MR400ToInf_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 30,100,400);
+  TH1D *MGG_MR150ToInf_Rsq0p00ToInf = new TH1D("MGG_MR150ToInf_Rsq0p00ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 20,100,400);
+  TH1D *MGG_MR300To400_Rsq0p05ToInf = new TH1D("MGG_MR300To400_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 20,100,400);
+  TH1D *MGG_MR400ToInf_Rsq0p05ToInf = new TH1D("MGG_MR400ToInf_Rsq0p05ToInf",";m_{#gamma#gamma} (GeV); Fraction ", 20,100,400);
 
   string categoryCutString = "";
-  if (category == "highpt") categoryCutString = " && ptgg >= 110 ";
+  if (category == "highpt") categoryCutString = " && ptgg >= 80 ";
   else if (category == "highres") categoryCutString = " && ptgg < 110 && pho1_sigEoE < 0.015 && pho2_sigEoE < 0.015 ";
   else if (category == "lowres") categoryCutString = " && ptgg < 110 && !(pho1_sigEoE < 0.015 && pho2_sigEoE < 0.015) ";
 
   string controlRegionCutString = "";
+  //if (useMC) controlRegionCutString = "&& pho1_pass_iso && pho2_pass_iso";
   if (useMC) controlRegionCutString = "";
   else controlRegionCutString = "&& (!pho1_pass_iso || !pho2_pass_iso) ";
-
+  // else controlRegionCutString = "&& pho1_pass_iso && pho2_pass_iso ";
+  //else controlRegionCutString = "&& pho1_pass_iso && pho2_pass_iso";
 
   tree->Draw("mgg>>MGG_MR150ToInf_Rsq0p00ToInf",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 150 && t1Rsq > 0.00 " + categoryCutString + controlRegionCutString).c_str(),"");
-  tree->Draw("mgg>>MGG_MR150To200_Rsq0p05ToInf",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 150 && MR < 200 && t1Rsq > 0.05 " + categoryCutString + controlRegionCutString).c_str(),"");
-  tree->Draw("mgg>>MGG_MR200To300_Rsq0p05ToInf",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 200 && MR < 300 && t1Rsq > 0.05 " + categoryCutString + controlRegionCutString).c_str(),"");
   tree->Draw("mgg>>MGG_MR300To400_Rsq0p05ToInf",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 300 && MR < 400 && t1Rsq > 0.05 " + categoryCutString + controlRegionCutString).c_str(),"");
   tree->Draw("mgg>>MGG_MR400ToInf_Rsq0p05ToInf",("abs(mbb-125)>=25 && abs(mbb-91.2)>=25 &&  ptgg > 20 && abs(pho1_eta) <1.44 && abs(pho2_eta)<1.44 && (pho1_pt>40||pho2_pt>40)&&pho1_pt>25 && pho2_pt>25 && pho1_pass_id && pho2_pass_id && mgg>100&&mgg<400 && MR > 400 && t1Rsq > 0.05 " + categoryCutString + controlRegionCutString).c_str(),"");
 
 
 
+
  NormalizeHist(MGG_MR150ToInf_Rsq0p00ToInf);
- NormalizeHist(MGG_MR150To200_Rsq0p05ToInf);
- NormalizeHist(MGG_MR200To300_Rsq0p05ToInf);
  NormalizeHist(MGG_MR300To400_Rsq0p05ToInf);
  NormalizeHist(MGG_MR400ToInf_Rsq0p05ToInf);
 
@@ -183,33 +182,26 @@ void PlotMggShapeVsMR(bool useMC = false, string category = "highres") {
  TLatex *tex = 0;
 
  cv = new TCanvas("cv","cv", 800,600); 
- MGG_MR150To200_Rsq0p05ToInf->SetStats(0);
- MGG_MR200To300_Rsq0p05ToInf->SetStats(0);
+ MGG_MR150ToInf_Rsq0p00ToInf->SetStats(0);
  MGG_MR300To400_Rsq0p05ToInf->SetStats(0);
  MGG_MR400ToInf_Rsq0p05ToInf->SetStats(0);
- MGG_MR150To200_Rsq0p05ToInf->Draw("hist");
- MGG_MR150To200_Rsq0p05ToInf->SetMaximum(0.3);
- MGG_MR150To200_Rsq0p05ToInf->SetMinimum(0.0);
- MGG_MR150To200_Rsq0p05ToInf->GetXaxis()->SetTitleSize(0.045);
- MGG_MR150To200_Rsq0p05ToInf->GetYaxis()->SetTitleSize(0.045);
- MGG_MR150To200_Rsq0p05ToInf->GetYaxis()->SetTitleOffset(0.8);
+ MGG_MR150ToInf_Rsq0p00ToInf->Draw("hist");
+ MGG_MR150ToInf_Rsq0p00ToInf->SetMaximum(0.25);
+ MGG_MR150ToInf_Rsq0p00ToInf->SetMinimum(0.0);
+ MGG_MR150ToInf_Rsq0p00ToInf->GetXaxis()->SetTitleSize(0.045);
+ MGG_MR150ToInf_Rsq0p00ToInf->GetYaxis()->SetTitleSize(0.045);
+ MGG_MR150ToInf_Rsq0p00ToInf->GetYaxis()->SetTitleOffset(0.8);
  
- MGG_MR150To200_Rsq0p05ToInf->SetLineColor(kBlack); 
- MGG_MR150To200_Rsq0p05ToInf->SetLineWidth(3); 
- MGG_MR150To200_Rsq0p05ToInf->SetMarkerStyle(8); 
- MGG_MR150To200_Rsq0p05ToInf->SetMarkerSize(1.3); 
- MGG_MR150To200_Rsq0p05ToInf->SetMarkerColor(kBlack); 
+ MGG_MR150ToInf_Rsq0p00ToInf->SetLineColor(kMagenta); 
+ MGG_MR150ToInf_Rsq0p00ToInf->SetLineWidth(4); 
+ MGG_MR150ToInf_Rsq0p00ToInf->SetMarkerStyle(8); 
+ MGG_MR150ToInf_Rsq0p00ToInf->SetMarkerSize(1.3); 
+ MGG_MR150ToInf_Rsq0p00ToInf->SetMarkerColor(kBlack); 
  
-
- MGG_MR200To300_Rsq0p05ToInf->SetLineWidth(3); 
  MGG_MR300To400_Rsq0p05ToInf->SetLineWidth(2); 
  MGG_MR400ToInf_Rsq0p05ToInf->SetLineWidth(2); 
- MGG_MR150ToInf_Rsq0p00ToInf->SetLineWidth(4); 
- MGG_MR200To300_Rsq0p05ToInf->SetLineColor(kRed);
  MGG_MR300To400_Rsq0p05ToInf->SetLineColor(kBlue);
  MGG_MR400ToInf_Rsq0p05ToInf->SetLineColor(kGreen+2);
- MGG_MR150ToInf_Rsq0p00ToInf->SetLineColor(kMagenta); 
- MGG_MR200To300_Rsq0p05ToInf->Draw("histsame");
  MGG_MR300To400_Rsq0p05ToInf->Draw("e1same");
  TH1D* MGG_MR400ToInf_Rsq0p05ToInf_Clone = (TH1D*)MGG_MR400ToInf_Rsq0p05ToInf->Clone();
  MGG_MR400ToInf_Rsq0p05ToInf_Clone->SetFillColor(kGreen+2);
@@ -223,8 +215,6 @@ void PlotMggShapeVsMR(bool useMC = false, string category = "highres") {
  legend->SetBorderSize(0);
  legend->SetFillStyle(0);
  legend->AddEntry(MGG_MR150ToInf_Rsq0p00ToInf , "MR > 150 & Inclusive in R^{2}" , "L");
- legend->AddEntry(MGG_MR150To200_Rsq0p05ToInf , "MR : (150,200) & R^{2} > 0.05" , "L");
- legend->AddEntry(MGG_MR200To300_Rsq0p05ToInf , "MR : (200,300) & R^{2} > 0.05" , "L");
  legend->AddEntry(MGG_MR300To400_Rsq0p05ToInf , "MR : (300,400) & R^{2} > 0.05" , "L");
  legend->AddEntry(MGG_MR400ToInf_Rsq0p05ToInf , "MR : (400,Inf) & R^{2} > 0.05" , "L");
  legend->Draw();
@@ -254,20 +244,13 @@ void PlotMggShapeVsMR(bool useMC = false, string category = "highres") {
 
 
 
-void MggShapeStudy() {
+void HighPtMggShapeStudy() {
 
-  PlotMggShapeVsRsq(false, "highres");
-  PlotMggShapeVsRsq(false, "lowres");
-  PlotMggShapeVsRsq(false, "highpt");
-  PlotMggShapeVsMR(false, "highres");
-  PlotMggShapeVsMR(false, "lowres");
+  //PlotMggShapeVsRsq(false, "highpt");
+  //PlotMggShapeVsRsq(true, "highpt");
+
+
   PlotMggShapeVsMR(false, "highpt");
-
-  PlotMggShapeVsRsq(true, "highres");
-  PlotMggShapeVsRsq(true, "lowres");
-  PlotMggShapeVsRsq(true, "highpt");
-  PlotMggShapeVsMR(true, "highres");
-  PlotMggShapeVsMR(true, "lowres");
   PlotMggShapeVsMR(true, "highpt");
 
 }
